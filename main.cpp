6dd4848fd44c3d28a339a0c18aced42035f292a9
Hudd <haddayn@gmail.com>
2022-10-09 11:02:43 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+#if 0
+	const auto options = parse_command_line(argc, argv, {
+		{ 'h', "host to connect to the FTP server (default localhost)" },
+		{ 'p', "port to connect to the FTP server (default 21)" },
+		{ 'U', "username" },
+		{ 'P', "password" },
+	});
+#endif
+
+	return 0;
+}
